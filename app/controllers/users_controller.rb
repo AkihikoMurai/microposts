b5b2db1de556483b54f3e33dd04e0388ced5a2d1
akihikomurai <glay.rock.around.the.world@gmail.com>
2015-12-07 04:53:49 +0000

    user-profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
-  
+  before_action :set_user, only:[:show, :edit, :update]
+  before_action :user_check, only:[:edit, :update]
+
   def show
-    @user = User.find(params[:id])
   end
   
   def new
@@ -17,6 +18,9 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+
+  def edit
+  end  
   
   private
   
@@ -24,4 +28,15 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
   
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_check
+    if current_user!=@user#current_userが@userと違ったら
+      #TOPへリダイレクト
+      redirect_to root_path
+    end
+    #redirect_to root_path unless current_user == @user
+  end
 end
